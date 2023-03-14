2fa03ed925a82cae5fb904f563de70ad8c39f26f
koalaSunlight <1937902984@qq.com>
2023-03-14 08:54:32 +0800

    Update

--- CrampedRoomOfDeath/application.11ec7.js
+++ CrampedRoomOfDeath/application.11ec7.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.e21e8.json';
+          this.settingsPath = 'src/settings.be0bb.json';
           this.showFPS = false;
         }
 
--- CrampedRoomOfDeath/index.67244.js
+++ CrampedRoomOfDeath/index.67244.js
@@ -1,4 +1,4 @@
-System.register(["./application.2fe5f.js"], function (_export, _context) {
+System.register(["./application.11ec7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.2fe5f.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application2fe5fJs) {
-      Application = _application2fe5fJs.Application;
+    setters: [function (_application11ec7Js) {
+      Application = _application11ec7Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
